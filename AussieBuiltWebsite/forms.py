a1ef8394a3b5aa17d86cc853dfcd95ef41aace58
Phoenix Richards <aussiebuiltsender@gmail.com>
2023-02-05 11:27:54 -0500

    Removed stuff relating to Google Recaptcha

--- AussieBuiltWebsite/forms.py
+++ AussieBuiltWebsite/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from captcha.fields import ReCaptchaField
 
 class EmailForm(forms.Form):
     email = forms.EmailField()
 
-class FormWithCaptcha(forms.Form):
-    captcha = ReCaptchaField()
--- test.py
+++
@@ -1,6 +0,0 @@
-import secrets
-allowed_chars = [chr(i) for i in range(0x21, 0x7F)]
-key_length = 80
-key = ''.join(secrets.choice(allowed_chars) for i in range(key_length))
-
-print(key)
