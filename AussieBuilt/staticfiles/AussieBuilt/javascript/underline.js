66d993ee519d616865db2c46f8ee4ee16497a6f8
Phoenix Richards <aussiebuiltsender@gmail.com>
2023-02-21 20:33:39 -0500

    Changed font size of text on about page, stopped logging things to console in javascript file and changed text on about page slightly.

--- AussieBuilt/staticfiles/AussieBuilt/javascript/underline.js
+++ AussieBuilt/staticfiles/AussieBuilt/javascript/underline.js
@@ -6,9 +6,9 @@ navLinks = document.getElementsByClassName('nav-links')
 navLinksText = document.getElementsByClassName('nav-link-text')
 navLinksAfter = document.getElementsByClassName('nav-links::after')
 
-console.log(navLinks)
-console.log(navLinksText)
-console.log(navLinksAfter)
+//console.log(navLinks)
+//console.log(navLinksText)
+//console.log(navLinksAfter)
 
 for (var count = 0; count < navLinks.length; count++) {
 
@@ -18,9 +18,9 @@ for (var count = 0; count < navLinks.length; count++) {
   var navLinkInfo = navLink.getBoundingClientRect();
   var navLinkWidth = navLinkInfo.width;
 
-  console.log(navLinkWidth)
-  console.log(`offsetwidth: ${navLink.offsetWidth}`)
-  console.log(`OFFSET WIDTH: ${navLinksText[count]['offsetPosition']}`)
+  //console.log(navLinkWidth)
+  //console.log(`offsetwidth: ${navLink.offsetWidth}`)
+  //console.log(`OFFSET WIDTH: ${navLinksText[count]['offsetPosition']}`)
 
   navLink.style.setProperty(`--navLink${count}`, `${navLinkWidth}px`)
 
